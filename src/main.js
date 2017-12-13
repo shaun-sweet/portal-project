d7ced665b6b09e20f685f585d1d0c04cc9582421
Shaun Sweet <shaun@shaunsweet.com>
2017-12-13 14:12:14 -0700

    refactor: move firebase config to env vars

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import 'firebaseui/dist/firebaseui.css'
 
 Vue.config.productionTip = false
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
