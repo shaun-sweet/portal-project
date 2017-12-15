03aa0176f1bc1b156259a9dc1b1a971d1b3897d9
Shaun Sweet <shaun@shaunsweet.com>
2017-12-15 03:39:04 -0700

    chore: rework precommit hook

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -2,16 +2,30 @@
 
   <main>
     <p></p>
-    <home-c test='WELCOME'>
+                                                            <home-c test='WELCOME'>
+              <button class="button">Click to disable</button>
+            </home-c>
+            <button>Test</button>
+            <p></p>
+            <home-c test='WELCOME'>
       <button class="button">Click to disable</button>
     </home-c>
-    <button>Test</button>
-  </main>
-
+      <button>Test</button>
+      <p></p>
+      <home-c test='WELCOME'>
+        <button class="button">Click to disable</button>
+</home-c>
+<button>Test</button>
+<p></p>
+<home-c test='WELCOME'>
+        <button class="button">Click to disable</button>
+      </home-c>
+      <button>Test</button>
+      
+    </main>
 </template>
 
-<script>
-export default { props: [ 'test' ] }
+<script>export default { props: [ 'test' ] }
 </script>
 
 <style lang="sass" scoped>
