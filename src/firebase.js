28b17c6947f060bd98f344eccd7960f61ad5990e
Shaun Sweet <shaun@shaunsweet.com>
2017-12-17 16:46:33 -0700

    feat: add firebase injection to vue instance

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,20 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 
-firebase.initializeApp(process.env.firebaseConfig)
+const firebaseConfig = process.env.firebaseConfig
+firebase.initializeApp(firebaseConfig)
 
-export const db = firebase.firestore()
+// https://vuejs.org/v2/guide/plugins.html
+export const Firestore = {
+  firestore: firebase.firestore(),
+  install (Vue, options) {
+    Vue.prototype.$firestore = this.firestore
+  },
+}
+
+export const Auth = {
+  auth: firebase.auth(),
+  install (Vue, options) {
+    Vue.prototype.$auth = this.auth
+  },
+}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './containers/App'
+import router from '@/router'
+import { Firestore, Auth } from '@/firebase'
 // import 'firebaseui/dist/firebaseui.css'
 import 'bulma/bulma.sass'
 
 Vue.config.productionTip = false
+
+Vue.use(Firestore)
+Vue.use(Auth)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
