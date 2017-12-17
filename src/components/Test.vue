0b38c395156766c63ee621bf0d67edf58717e97b
Shaun Sweet <shaun@shaunsweet.com>
2017-12-17 14:18:11 -0700

    test: test commit

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,31 +1,46 @@
 <template lang="html">
-
   <main>
+
+
+
     <p></p>
-                                                            <home-c test='WELCOME'>
-              <button class="button">Click to disable</button>
-            </home-c>
-            <button>Test</button>
-            <p></p>
-            <home-c test='WELCOME'>
+    <home-c test='WELCOME'>
+
+
+
       <button class="button">Click to disable</button>
     </home-c>
-      <button>Test</button>
-      <p></p>
-      <home-c test='WELCOME'>
-        <button class="button">Click to disable</button>
-</home-c>
-<button>Test</button>
-<p></p>
-<home-c test='WELCOME'>
-        <button class="button">Click to disable</button>
-      </home-c>
-      <button>Test</button>
+    <button>Test</button>
+    <p></p>
+
+
+
+    <home-c test='WELCOME'>
+      <button class="button">Click to disable</button>
+
+
+    </home-c>
+    <button>Test</button>
+    <p></p>
+    <home-c test='WELCOME'>
+      <button class="button">Click to disable</button>
+    </home-c>
+
+
+    <button>Test</button>
+    <p></p>
+    <home-c test='WELCOME'>
+
+
       
-    </main>
+      <button class="button">Click to disable</button>
+    </home-c>
+    <button>Test</button>
+  </main>
 </template>
 
-<script>export default { props: [ 'test' ] }
+<script>
+export default { props: [ 'test' ] }
 </script>
 
 <style lang="sass" scoped>
