6a0697e2318cecaaef6fed7163f46a301277640c
Shaun Sweet <shaun@shaunsweet.com>
2017-12-17 17:47:31 -0700

    test: linter with footer

---
+++ src/containers/Footer.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">This the footer</div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'footer',
+}
+</script>
+
+<style lang="sass">
+</style>
