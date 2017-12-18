6a0196ea758a86a1d858f8f6a606f0ee0aed9786
Shaun Sweet <shaun@shaunsweet.com>
2017-12-17 18:12:05 -0700

    feat: add login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>this be a login component</div>
+  <div>
+    <div class="field">
+      <label class="label">Name</label>
+      <div class="control">
+        <input class="input" type="text" placeholder="Text input">
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Username</label>
+      <div class="control has-icons-left has-icons-right">
+        <input class="input is-success" type="text" placeholder="Text input" value="bulma">
+        <span class="icon is-small is-left">
+          <i class="fa fa-user"></i>
+        </span>
+        <span class="icon is-small is-right">
+          <i class="fa fa-check"></i>
+        </span>
+      </div>
+      <p class="help is-success">This username is available</p>
+    </div>
+  </div>
 </template>
 
 <script>
