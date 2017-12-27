dd44a2866745f49f447c3f2e3511d47fdf8a8499
Talal Talhouk <talal@sbcglobal.net>
2017-12-27 09:00:03 -0800

    feat: add profile routing and guards

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <h1>Profile Page</h1>
+    <router-link :to="'/profile/' + $auth.currentUser.uid ">My Profile</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Home from '@/pages/Home'
 import Login from '@/pages/Login'
 import Signup from '@/pages/Signup'
 import Profile from '@/pages/Profile'
+import ProfileDetail from '@/pages/ProfileDetail'
+import ProfileEdit from '@/pages/ProfileEdit'
 import Messages from '@/pages/Messages'
 import Projects from '@/pages/Projects'
 import Secret from '@/pages/Secret'
@@ -38,8 +40,27 @@ let router = new Router({
     },
     {
       path: '/profile',
-      name: 'Home',
+      name: 'Profile',
       component: Profile,
+      meta: {
+        requiresAuth: true,
+      },
+      children: [
+        { path: ':id', component: ProfileDetail },
+        {
+          path: ':id/edit',
+          component: ProfileEdit,
+          beforeEnter (to, from, next) {
+            const routeId = to.params.id
+            const currentUser = Auth.auth.currentUser
+            if (routeId !== currentUser.uid) {
+              next(`/profile/${routeId}`)
+            } else {
+              next()
+            }
+          },
+        },
+      ],
     },
     {
       path: '/messages',
@@ -55,7 +76,7 @@ let router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
-  let currentUser = Auth.auth.currentUser
+  const currentUser = Auth.auth.currentUser
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
   if (requiresAuth && !currentUser) {
     console.log('no user logged in')
